8324a914ad3b60746d310eb3e9c7aed836cb9fa2
Albert Sadykov <albertsadykov@ro.ru>
2021-09-28 18:05:20 +0300

    templates

---
+++ components/single-file-component.vue
@@ -0,0 +1,20 @@
+<template>
+		<div class="hello">
+				{{ msg }}
+		</div>
+</template>
+
+<script>
+		export default {
+				name: 'hello',
+				data() {
+						return {
+								msg: 'Welcome to Vue.js App'
+						}
+				}
+		}
+</script>
+
+<!-- scoped - ограничивает область видимости стилей текущим копонентом -->
+<style scoped> 
+</style>
