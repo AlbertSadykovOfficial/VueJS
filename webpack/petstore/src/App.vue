248b7a3ea0bc630b57008ab2fc9227b3a2159bd7
Albert Sadykov <albertsadykov@ro.ru>
2021-10-01 21:23:07 +0300

    Webpack

--- webpack/petstore/src/App.vue
+++ webpack/petstore/src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    <router-view/>
+    <transition name="fade" mode="out-in">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
@@ -11,12 +13,20 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+      font-family: 'Avenir', Helvetica, Arial, sans-serif;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+      text-align: center;
+      color: #2c3e50;
+      margin-top: 60px;
+    }
+
+    .fade-enter-active, .fade-leave-active {
+        transition: opacity .5s ease-out;
+    }
+
+    .fade-enter, .fade-leave-to {
+        opacity: 0;
+    }
 </style>
--- webpack/petstore/src/components/Main.vue
+++ webpack/petstore/src/components/Main.vue
@@ -29,25 +29,41 @@
           v-if="canAddToCart(product)">Add to cart</button>
           <button disabled="true" class=" btn btn-primary btn-lg"
           v-else >Add to cart</button>
-          <span class="inventory-message"
-          v-if="product.availableInventory - cartCount(product.id) === 0">All Out!
-        </span>
-        <span class="inventory-message"
-        v-else-if="product.availableInventory - cartCount(product.id) < 5">
-        Only {{product.availableInventory - cartCount(product.id)}} left!
-      </span>
-      <span class="inventory-message"
-      v-else>Buy Now!
-    </span>
-    <div class="rating">
-      <span  v-bind:class="{'rating-active' :checkRating(n, product)}"
-      v-for="n in 5" >☆
-    </span>
-  </div>
-</div><!-- end of col-md-6-->
-</div><!-- end of row-->
-<hr />
-</div><!-- end of v-for-->
+
+<!-- Применение анимации  
+      
+      Атрибут key нужен для преобразваня несколких элементов с одинаковыми тегами,
+      рекомендуется добавляь его в ходе работы с нескольими элементами внутри 
+      компонента transition
+-->
+          <transition name="bounce" mode="out-in"
+              <span class="inventory-message"
+                    v-if="product.availableInventory - cartCount(product.id) === 0"
+                    key="0">
+                  All Out!
+              </span>
+              <span class="inventory-message"
+                    v-else-if="product.availableInventory - cartCount(product.id) < 5"
+                    key="1">
+                  Only {{product.availableInventory - cartCount(product.id)}} left!
+              </span>
+              <span class="inventory-message"
+                    v-else
+                    key="2">
+                  Buy Now!
+              </span>
+          </transition> 
+          
+
+          <div class="rating">
+              <span  v-bind:class="{'rating-active' :checkRating(n, product)}"
+                     v-for="n in 5" >☆
+              </span>
+          </div>
+      </div><!-- end of col-md-6-->
+  </div><!-- end of row-->
+  <hr />
+  </div><!-- end of v-for-->
 </main>
 </div>
 </template>
@@ -135,5 +151,30 @@ export default {
   }
 };
 </script>
+
+
 <style scoped>
+
+    .bounce-enter-active {
+      animation: shake 0.72s cubic-bezier(.37,.07,.19,.97) both;
+      transform: translate3d(0, 0, 0);
+      backface-visibility: hidden;
+    }
+
+    @keyframes shake {
+      10%, 90% {
+        color: red;
+        transform: translate3d(-1px, 0, 0);
+      }
+      20%, 80% {
+        transform: translate3d(2px, 0, 0);
+      }
+      30%, 50%, 70% {
+        color: red;
+        transform: translate3d(-4px, 0, 0);
+      }
+      40%, 60% {
+        transform: translate3d(4px, 0, 0);
+      }
+    }
 </style>
